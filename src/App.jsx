d248c3b4bf758aa4ed0c3cf0e2cc29d57bd18129
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-05-19 17:43:32 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ArticleList from "./components/ArticleList/ArticleList"
 import Header from "./components/Header/Header"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Header/>
+      <ArticleList/>
     </>
   )
 }
